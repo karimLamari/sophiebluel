07fc68da2b9b3bb40c3473f02007c47da5d222c1
karimLamari <l.karim9942@gmail.com>
2025-08-09 13:14:26 +0200

    effacement instantané a la suppression d'un work

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -1,36 +1,37 @@
 // Fonction d'initialisation admin
 function initAdmin() {
-  const categories = document.querySelector('.categories');
+  const categories = document.querySelector(".categories");
 
-  const token = localStorage.getItem('token');
-    if (!token){
-      categories.style.display = 'flex';
-      return;
-    };
-    categories.style.display = 'none';
-    // Bouton modifier
-    const portfolioTitle = document.getElementById('portfolio-title');
-    if (portfolioTitle) {
-      const modifyButton = document.createElement('button');
-      modifyButton.className = 'modify-button';
-      modifyButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> modifier';
-      portfolioTitle.appendChild(modifyButton);
-      modifyButton.addEventListener('click', createModal);
-    }
+  const token = localStorage.getItem("token");
+  if (!token) {
+    categories.style.display = "flex";
+    return;
   }
-  
-  // Nettoyage de l'interface admin
-  function cleanAdminInterface() {
-    document.querySelector('.modify-button')?.remove();
+  categories.style.display = "none";
+  // Bouton modifier
+  const portfolioTitle = document.getElementById("portfolio-title");
+  if (portfolioTitle) {
+    const modifyButton = document.createElement("button");
+    modifyButton.className = "modify-button";
+    modifyButton.innerHTML =
+      '<i class="fa-regular fa-pen-to-square"></i> modifier';
+    portfolioTitle.appendChild(modifyButton);
+    modifyButton.addEventListener("click", createModal);
   }
-  
+}
+
+// Nettoyage de l'interface admin
+function cleanAdminInterface() {
+  document.querySelector(".modify-button")?.remove();
+}
+
 function createModal() {
-  const overlay = document.createElement('div');
-  overlay.className = 'overlay';
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
   document.body.appendChild(overlay);
 
-  const modal = document.createElement('div');
-  modal.className = 'modal';
+  const modal = document.createElement("div");
+  modal.className = "modal";
   modal.innerHTML = `
   <div class="modal-content">
     <h2>Galerie photo</h2>
@@ -41,52 +42,52 @@ function createModal() {
   `;
   document.body.appendChild(modal);
 
-  //fermer modal  
-  overlay.addEventListener('click', () => {
+  //fermer modal
+  overlay.addEventListener("click", () => {
     overlay.remove();
     modal.remove();
   });
-  modal.querySelector('.close-modal').addEventListener('click', () => {
+  modal.querySelector(".close-modal").addEventListener("click", () => {
     overlay.remove();
     modal.remove();
   });
-   //chargement des photos
-   getWorks(modal.querySelector('.modal-content-photos'));
+  //chargement des photos
+  getWorks(modal.querySelector(".modal-content-photos"));
 
-   //ajouter une photo
-   modal.querySelector('.add-button').addEventListener('click', () => {
+  //ajouter une photo
+  modal.querySelector(".add-button").addEventListener("click", () => {
     modal.remove();
     overlay.remove();
     createModalAddPhoto();
-   });
+  });
 }
 
 async function getWorks(modalContentPhotos) {
-  const gallery = await fetch('http://localhost:5678/api/works');
+  const gallery = await fetch("http://localhost:5678/api/works");
   const works = await gallery.json();
-  modalContentPhotos.innerHTML = '';
-  works.forEach(work => {
-    workContainer = document.createElement('div');
-    workContainer.className = 'work-container';
+  modalContentPhotos.innerHTML = "";
+  works.forEach((work) => {
+    workContainer = document.createElement("div");
+    workContainer.className = "work-container";
     workContainer.innerHTML = `
     <img src="${work.imageUrl}" alt="${work.title}">
     <button class="delete-button" data-id="${work.id}"><i class="fa-solid fa-trash-can"></i></button>
     `;
     modalContentPhotos.appendChild(workContainer);
 
-    workContainer.querySelector('.delete-button').addEventListener('click', () => {
-      deleteWork(work.id);
-    });
-
-  })
-
+    workContainer
+      .querySelector(".delete-button")
+      .addEventListener("click", () => {
+        deleteWork(work.id);
+      });
+  });
 }
 function createModalAddPhoto() {
-  const overlay = document.createElement('div');
-  overlay.className = 'overlay';
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
   document.body.appendChild(overlay);
-  const modal = document.createElement('div');
-  modal.className = 'modal';
+  const modal = document.createElement("div");
+  modal.className = "modal";
   modal.innerHTML = `
       <button class="prev-button"><i class="fa-solid fa-arrow-left"></i></button>
       <h2>Ajout photo</h2>
@@ -114,107 +115,107 @@ function createModalAddPhoto() {
       <button type="submit" class="submit-btn">Valider</button>
     </form>
   `;
-  fetch('http://localhost:5678/api/categories')
-  .then(response => response.json())
-  .then(categories => {
-    const select = modal.querySelector('#category');
-    categories.forEach(category => {
-      const option = document.createElement('option');
-      option.value = category.id;
-      option.textContent = category.name;
-      select.appendChild(option);
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((categories) => {
+      const select = modal.querySelector("#category");
+      categories.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.id;
+        option.textContent = category.name;
+        select.appendChild(option);
+      });
     });
-  });
-  
+
   document.body.appendChild(modal);
   //bouton retour
-  modal.querySelector('.prev-button').addEventListener('click', () => {
+  modal.querySelector(".prev-button").addEventListener("click", () => {
     modal.remove();
     overlay.remove();
     createModal();
   });
   //fermer modal
-  modal.querySelector('.close-modal').addEventListener('click', () => {
+  modal.querySelector(".close-modal").addEventListener("click", () => {
     modal.remove();
     overlay.remove();
   });
   //ajouter une photo
-  const button = modal.querySelector('.upload-btn');
-  button.addEventListener('click', () => {
-    const fileInput = modal.querySelector('#photo-input');
+  const button = modal.querySelector(".upload-btn");
+  button.addEventListener("click", () => {
+    const fileInput = modal.querySelector("#photo-input");
     fileInput.click();
   });
-    // Gestion de la prévisualisation de l'image
-    const photoInput = modal.querySelector('#photo-input');
-    const imagePreview = modal.querySelector('#image-preview');
-    const uploadIcon = modal.querySelector('.fa-image');
-    const uploadBtn = modal.querySelector('.upload-btn');
-    const uploadText = modal.querySelector('.image-upload p')
-  photoInput.addEventListener('change', function(e) {
+  // Gestion de la prévisualisation de l'image
+  const photoInput = modal.querySelector("#photo-input");
+  const imagePreview = modal.querySelector("#image-preview");
+  const uploadIcon = modal.querySelector(".fa-image");
+  const uploadBtn = modal.querySelector(".upload-btn");
+  const uploadText = modal.querySelector(".image-upload p");
+  photoInput.addEventListener("change", function (e) {
     if (this.files && this.files[0]) {
       const reader = new FileReader();
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         imagePreview.src = e.target.result;
-        imagePreview.style.display = 'block';
-        uploadIcon.style.display = 'none';
-        uploadBtn.style.display = 'none';
-        uploadText.style.display = 'none';
+        imagePreview.style.display = "block";
+        uploadIcon.style.display = "none";
+        uploadBtn.style.display = "none";
+        uploadText.style.display = "none";
       };
       reader.readAsDataURL(this.files[0]);
     }
   });
-  modal.querySelector('#add-photo-form').addEventListener('submit', (e) => {
+  modal.querySelector("#add-photo-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     const formData = new FormData();
     console.log(photoInput.files[0]);
-    formData.append('image', photoInput.files[0]);
-    formData.append('title', modal.querySelector('#title').value);
-    formData.append('category', modal.querySelector('#category').value);
+    formData.append("image", photoInput.files[0]);
+    formData.append("title", modal.querySelector("#title").value);
+    formData.append("category", modal.querySelector("#category").value);
     console.log([...formData.entries()]); // Pour déboguer
 
     addWork(formData);
   });
-  }
+}
 function updateModalAddPhoto() {
-  const overlay = document.createElement('div');
-  overlay.className = 'overlay';
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
   document.body.appendChild(overlay);
-  const modal = document.createElement('div');
-  modal.className = 'modal';
+  const modal = document.createElement("div");
+  modal.className = "modal";
 }
 async function deleteWork(id) {
+  const modalContentPhotos = document.querySelector(".modal-content-photos");
   const response = await fetch(`http://localhost:5678/api/works/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'Authorization': `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
   if (response.ok) {
-    alert('Photo supprimée avec succès');
+    alert("Photo supprimée avec succès");
     getWorks(modalContentPhotos);
   } else {
-    alert('Erreur lors de la suppression de la photo');
+    alert("Erreur lors de la suppression de la photo");
   }
 }
 async function addWork(work) {
-  const token = localStorage.getItem('token'); 
-  const response = await fetch('http://localhost:5678/api/works', {
-    method: 'POST',
+  const token = localStorage.getItem("token");
+  const response = await fetch("http://localhost:5678/api/works", {
+    method: "POST",
     headers: {
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    body: work
+    body: work,
   });
   if (response.ok) {
-    alert('Photo ajoutée avec succès');
-    window.location.href = 'index.html';
-    
+    alert("Photo ajoutée avec succès");
+    window.location.href = "index.html";
   } else {
-    alert('Erreur lors de l\'ajout de la photo');
+    alert("Erreur lors de l'ajout de la photo");
   }
 }
-  // Initialisation au chargement
-  document.addEventListener('DOMContentLoaded', function() {
-    initAdmin();
-  });
+// Initialisation au chargement
+document.addEventListener("DOMContentLoaded", function () {
+  initAdmin();
+});
